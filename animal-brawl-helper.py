dfc28507ad85ecbdc66d81f68fc5f72006e5897c
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2025-01-23 16:19:34 +0100

    Solucionado un problema a la hora de saber donde el fichero con los certificados SSL

--- animal-brawl-helper.py
+++ animal-brawl-helper.py
@@ -1,3 +1,7 @@
+from os import path, environ
+
+LOCAL_DIR = path.dirname(path.realpath(__file__))
+environ['SSL_CERT_FILE'] = path.join(LOCAL_DIR, 'certifi', 'cacert.pem')
 from abh import run
 
 run()
